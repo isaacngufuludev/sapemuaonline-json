a34ff9b87063e59ceefea1ef7cf481b1ce7cc142
isaacngufuludev <isaacngufulu70@icloud.com>
2026-05-13 21:42:54 +0100

    fixing input in mobile 2

--- src/pages/auth/components/AuthInput.jsx
+++ src/pages/auth/components/AuthInput.jsx
@@ -9,7 +9,7 @@ function AuthInput({ value, id, onChange, type, name, ...inputProps }) {
       className={`dark:bg-gray-800 dark:text-white relative  dark:border-gray-700 overflow-hidden  w-full border border-stone-300 rounded-xl text-xs md:text-sm ${isFocus && "border-blue-700 "} `}
     >
       <label
-        className={`absolute z-10 text-[15px] top-1/2 -translate-y-1/2 text-gray-600 dark:text-gray-300 transition-transform duration-300 translate-x-3 " ${(isFocus || value) && " md:text-sm"}`}
+        className={`absolute z-10 text-[15px] top-1/2 -translate-y-1/2 text-gray-600 dark:text-gray-300 transition-transform duration-300 translate-x-3 " ${(isFocus || value) && "-translate-y-[1.5rem] md:text-sm"}`}
         htmlFor={id}
       >
         {name}
